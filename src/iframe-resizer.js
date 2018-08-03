1138def812e79ad6a2a19f02177a4834076447ae
dumith-eranga <dumith.eranga@gmail.com>
2018-08-03 18:38:07 +0530

    Close the iFrameResizer on "$destroy"
    Close the iFrameResizer when the "$destroy" event is fired on the element.

--- src/iframe-resizer.js
+++ src/iframe-resizer.js
@@ -8,6 +8,10 @@
                 restrict: 'A',
                 link: function (scope, element) {
                     iFrameResize({}, element[0]);
+                    
+                    element.on('$destroy', function () {
+                        element[0].iFrameResizer.close();
+                    });
                 }
             };
         });
